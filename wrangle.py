7335480c3e19fcfc5cffec426afc5196bcd0db7a
Cris Giovanoni <giovanonicris@gmail.com>
2020-01-13 16:10:43 -0600

    changed wrangle

--- wrangle.py
+++ wrangle.py
@@ -3,11 +3,11 @@ import warnings
 warnings.filterwarnings("ignore")
 
 def get_train():
-    train = pd.read_csv('./data/train.csv')
+    train = pd.read_csv('./train.csv')
     return train
 
 def get_train_labels():
-    train_labels = pd.read_csv('./data/train_labels.csv')
+    train_labels = pd.read_csv('./train_labels.csv')
     return train_labels
 
 def get_assessment_users(cache=True):
